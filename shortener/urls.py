d8e810024ae00c500e511c80a13ee6c60a0622b7
Rashi <rashi.ranjan.cse22@itbhu.ac.in>
2023-11-01 18:49:49 +0530

    2nd commit

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('short-urls/', views.short_url_list, name='short_url_list'),
+    # url to view the list of urls
     path('', views.shorten_url, name='shorten_url'),
     path('<str:short_code>/', views.redirect_to_original_url, name='redirect_to_original_url'),
 
